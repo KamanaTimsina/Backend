668fd8faa7cd7fe945414f8645e1778ae1018588
kamanaith <timsinakamaksha@gmail.com>
2026-01-09 21:43:36 +0545

    backend frontend connected and proxy added

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
-app.get('/jokes',(req,res)=>{
+app.get('/api/jokes',(req,res)=>{ 
 const jokes  = [
     {
     id:1,
@@ -37,13 +37,13 @@ const jokes  = [
 res.json(jokes);
 });
 
-app.get('/login', (req, res) => {
-  res.send('Hello login page!')
-})
+// app.get('/login', (req, res) => {
+//   res.send('Hello login page!')
+// })
 
-app.get('/signup', (req, res) => {
-  res.send('Hello signup page!')
-})
+// app.get('/signup', (req, res) => {
+//   res.send('Hello signup page!')
+// })
 
 app.listen(PORT, () => {
   console.log(`serving at http://localhost:${PORT}`)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function App() {
+  const [Jokes,setJokes ] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) =>{
+      setJokes(response.data)
+    })
+    .catch((error) =>{
+      console.log(error);
+      
+    })
+  })
+
+  return (
+    <>
+     <h1>Frontend is running</h1>
+     <p>JOKES: {Jokes.length}</p>
+
+     {
+      Jokes.map((joke,index) =>(
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))
+     }
+    </>
+  )
+}
+
+export default App
